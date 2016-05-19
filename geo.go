751511a12153f6465ddafe38ad9bce2e6f17e366
Brian Sorahan <bsorahan@leaf.ag>
2016-05-18 23:02:50 -0500

    remove unused constants

--- geo.go
+++ geo.go
@@ -1,8 +1,3 @@
 // Package geo provides a small set of geometrical types
 // and operations on them.
 package geo
-
-const prefix = "[geo]"
-
-// Margin is a value used to fudge equality when two floats are very close to each other.
-var Margin = 1e-100
